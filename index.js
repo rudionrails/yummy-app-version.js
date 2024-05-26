2f5bdf245d75690dcae1187e2041184f38ac0aa9
Rudolf Schmidt <me@rudionrails.com>
2024-05-26 21:54:48 +0200

    feat!: Adds branch and prefix to options and changes defaults

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+"use strict";
+
 const { execSync } = require("child_process");
+
+function gitBranch() {
+  let branch;
+
+  try {
+    branch = execSync("git rev-parse --abbrev-ref HEAD").toString().trim();
+  } catch (_) {
+    branch = "main";
+  }
+
+  return branch;
+}
 
 function gitRevision() {
   let revision;
@@ -7,22 +21,63 @@ function gitRevision() {
     revision = execSync("git describe --tags --long")
       .toString()
       .match(/^(?<tag>.+)-(?<distance>\d+)-(?<hash>\w{8}).*/).groups;
-  } catch {
-    revision = { tag: "0.0.0", distance: "0", hash: "" };
+  } catch (_) {
+    revision = { tag: "0.0.0", distance: "0", hash: "00000000" };
   }
 
   return revision;
 }
 
+/**
+ * Generate the app version
+ *
+ * @param {object} options Configuration options
+ * @param {string} options.prefix - Adds a prefix to the version
+ * @param {boolean} options.banch - Adds the current git branch
+ * @param {boolean} options.distance - Adds the distance to the last git tag
+ * @param {boolean} options.hash - Adds the current git commit hash
+ *
+ * @returns {string} The generated app version
+ */
 function appVersion(options = {}) {
-  const revision = gitRevision();
-  const { year, month, day } = new Date()
-    .toISOString()
-    .match(/^(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})T.+$/).groups;
+  const opts = Object.assign(
+    {
+      prefix: "",
+      branch: true,
+      date: true,
+      distance: true,
+      hash: true,
+    },
+    options,
+  );
 
-  let version = `${revision.tag}-${year}${month}${day}`;
-  if (options.distance) version = `${version}.${revision.distance}`;
-  if (options.hash) version = `${version}.${revision.hash}`;
+  const date = new Date().toISOString().substring(0, 10).replaceAll("-", ""); // YYYMMDD
+  const revision = gitRevision(); // { tag, distance, hash }
+  const branch = gitBranch(); // main
+
+  let version = revision.tag;
+
+  if (opts.prefix !== "" && !version.startsWith(`${opts.prefix}+`)) {
+    version = `${opts.prefix}+${version}`;
+  }
+
+  if (opts.branch || opts.date) {
+    const branchOrDate = [];
+
+    if (opts.branch) branchOrDate.push(branch);
+    if (opts.date) branchOrDate.push(date);
+
+    version = `${version}+${branchOrDate.join(".")}`;
+  }
+
+  if (opts.distance || opts.hash) {
+    const distanceOrHash = [];
+
+    if (opts.distance) distanceOrHash.push(revision.distance);
+    if (opts.hash) distanceOrHash.push(revision.hash);
+
+    version = `${version}+${distanceOrHash.join(".")}`;
+  }
 
   return version;
 }
--- index.test.js
+++ index.test.js
@@ -22,8 +22,22 @@ afterAll(() => {
   jest.useRealTimers();
 });
 
+beforeEach(() => {
+  when(childProcess.execSync)
+    .calledWith("git describe --tags --long")
+    .mockReturnValue("1.2.3-7-g6cef876");
+
+  when(childProcess.execSync)
+    .calledWith("git rev-parse --abbrev-ref HEAD")
+    .mockReturnValue("main");
+});
+
 test("to be a function", () => {
   expect(typeof appVersion).toEqual("function");
+});
+
+test("to return the correct version", () => {
+  expect(appVersion()).toEqual(`1.2.3+main.${date}+7.g6cef876`);
 });
 
 test("to return the default version when git command fails", () => {
@@ -33,21 +47,51 @@ test("to return the default version when git command fails", () => {
       throw new Error("oopsy");
     });
 
-  expect(appVersion()).toEqual(`0.0.0-${date}`);
+  when(childProcess.execSync)
+    .calledWith("git rev-parse --abbrev-ref HEAD")
+    .mockImplementation(() => {
+      throw new Error("oopsy");
+    });
+
+  expect(appVersion()).toEqual(`0.0.0+main.${date}+0.00000000`);
 });
 
-test("to return the correct version when `distance = true` is passed", () => {
-  when(childProcess.execSync)
-    .calledWith("git describe --tags --long")
-    .mockReturnValue("1.2.3-7-g6cef876");
+describe("when options.prefix is passed", () => {
+  test("to return the correct version", () => {
+    when(childProcess.execSync)
+      .calledWith("git describe --tags --long")
+      .mockReturnValue("1.2.3-7-g6cef876");
 
-  expect(appVersion({ distance: true })).toEqual(`1.2.3-${date}.7`);
+    expect(appVersion({ prefix: "my-application" })).toEqual(
+      `my-application+1.2.3+main.${date}+7.g6cef876`,
+    );
+  });
+
+  test("to return the correct version if tag already has prefix", () => {
+    when(childProcess.execSync)
+      .calledWith("git describe --tags --long")
+      .mockReturnValue("my-application+1.2.3-7-g6cef876");
+
+    expect(appVersion({ prefix: "my-application" })).toEqual(
+      `my-application+1.2.3+main.${date}+7.g6cef876`,
+    );
+  });
 });
 
-test("to return the correct version when `hash = true` is passed", () => {
-  when(childProcess.execSync)
-    .calledWith("git describe --tags --long")
-    .mockReturnValue("1.2.3-7-g6cef876");
+test("to return the correct version when options.branch = false", () => {
+  expect(appVersion({ branch: false })).toEqual(`1.2.3+${date}+7.g6cef876`);
+});
 
-  expect(appVersion({ hash: true })).toEqual(`1.2.3-${date}.g6cef876`);
+test("to return the correct version when options.date = false", () => {
+  expect(appVersion({ date: false })).toEqual(`1.2.3+main+7.g6cef876`);
+});
+
+test("to return the correct version when options.distance = false", () => {
+  expect(appVersion({ distance: false })).toEqual(
+    `1.2.3+main.${date}+g6cef876`,
+  );
+});
+
+test("to return the correct version when options.hash = false", () => {
+  expect(appVersion({ hash: false })).toEqual(`1.2.3+main.${date}+7`);
 });
